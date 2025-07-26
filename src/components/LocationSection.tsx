70c701b274a8d39af0a2328759370819f4cb2f50
Leonardo Boschi <leoboschi8@gmail.com>
2025-07-26 19:06:09 +0200

    Undo Maps

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -97,7 +97,9 @@ const LocationSection = () => {
           </div>
           
           <div className="rounded-xl overflow-hidden shadow-md h-100 relative">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1978.2700157238503!2d9.28300037692117!3d45.70217648978248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786a4eb5b9bec2b%3A0xf4ec4677a37fa1a!2sStudio%20Odontoiatrico%20Dott.%20Alessio%20Rocca!5e1!3m2!1sit!2sit!4v1753548763566!5m2!1sit!2sit" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1978.2700157238503!2d9.28300037692117!3d45.70217648978248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786a4eb5b9bec2b%3A0xf4ec4677a37fa1a!2sStudio%20Odontoiatrico%20Dott.%20Alessio%20Rocca!5e1!3m2!1sit!2sit!4v1753549502148!5m2!1sit!2sit" width="100%" height="100%" style={{
+            border: 0
+          }} allowFullScreen={false} loading="lazy" title="Mappa dello Studio Odontoiatrico Rocca" className="absolute inset-0"></iframe>
           </div>
         </div>
       </div>
